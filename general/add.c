02a6bf4efa2381bcb0ae7b677696376e1b3bfaad
joshuamuthenya <joshuawambua514@gmail.com>
2026-05-03 21:38:40 +0300

    back from commitments

---
+++ general/add.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+printf("Adding two numbers.\n");
+float a,b;
+printf("Input number 1:\n");
+scanf("%f",&a);
+
+printf("Input number 2:\n");
+scanf("%f",&b);
+double sum=a+b;
+
+printf("sum is :%.2f",sum);
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
